b8b50c84498eec5521baf6a6261845accce077e5
warrenjramos <wjnramos@gmail.com>
2022-08-16 12:19:57 -0700

    Deleted the User page that came from the medium article in the ticket for a simple CRUD app. This isn't needed for the actual training.

--- src/webpages/index.js
+++ src/webpages/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import User from "./User";
 import CreatePost from "./CreatePost";
 
 const Webpages = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/user" element={<User />} />
             <Route path="/create-post" element={<CreatePost />} />
         </Routes>
     );
