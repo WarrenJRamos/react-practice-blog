2cb07d15b2a93401c463123d06ef36e12cbcfd88
warrenjramos <wjnramos@gmail.com>
2022-08-16 11:43:47 -0700

    Added the boilerplate for update post page. Might change this to instead bring up a popup modal.

---
+++ src/webpages/UpdatePost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Convert this into a modal after using React portals
+const UpdatePost = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default UpdatePost;
--- src/webpages/index.js
+++ src/webpages/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./home";
-import User from "./user";
+import Home from "./Home";
+import User from "./User";
+import CreatePost from "./CreatePost";
 
 const Webpages = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/user" element={<User />} />
+            <Route path="/create-post" element={<CreatePost />} />
         </Routes>
     );
 };
